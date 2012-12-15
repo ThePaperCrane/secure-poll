94b71b500073d44446e87ccf7410fa3c9a30b8ec
Rafic Melhem <raficmelhem@gmail.com>
2012-12-15 17:33:43 -0500

    Initial commit.;

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecurePoll::Application.config.secret_token = 'f75b8ed1bfdad44e081331889120b351a0b2c1f5d03292c97e764c37fda70334d314698cc02dcf94b093da51a1521d10f376df04a4e45b68151c3b1644410bb9'
