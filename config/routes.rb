940474e3176616695bda8962983c43bebadcb693
Rafic Melhem <raficmelhem@gmail.com>
2012-12-21 00:18:03 -0500

    user/admin authorization (not the same as authentication)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SecurePoll::Application.routes.draw do
+
   authenticated :user do
     root :to => 'home#index'
   end
@@ -6,6 +7,7 @@ SecurePoll::Application.routes.draw do
   root :to => "home#index"
 
   devise_for :users
+  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
